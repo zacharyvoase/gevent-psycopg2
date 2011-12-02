10d735887c45711c45bd1c1bef99b20ed4a8fa6c
Zachary Voase <z@zacharyvoase.com>
2011-12-02 05:34:22 +0000

    Bumped to v0.0.2.

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ from setuptools import setup, find_packages
 
 setup(
     name='gevent-psycopg2',
-    version='0.0.1',
+    version='0.0.2',
     description='pip-installable package for patching psycopg2 to use gevent',
     author='Zachary Voase',
     author_email='z@dvxhouse.com',
