19575dfada7848080e9de3845c9f474d9d29804e
Zachary Voase <z@zacharyvoase.com>
2011-12-02 00:25:37 +0000

    Renamed to gevent-psycopg2.

--- setup.py
+++ setup.py
@@ -8,12 +8,12 @@ from setuptools import setup, find_packages
 
 
 setup(
-    name='gevent_psycopg2',
+    name='gevent-psycopg2',
     version='0.0.1',
     description='pip-installable package for patching psycopg2 to use gevent',
     author='Zachary Voase',
     author_email='z@dvxhouse.com',
-    url='http://github.com/zacharyvoase/gevent_psycopg2',
+    url='http://github.com/zacharyvoase/gevent-psycopg2',
     packages=find_packages(where='lib'),
     package_dir={'': 'lib'},
     install_requires=[
